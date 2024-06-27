c3109028d2538f9280df0417446954280aa15cfd
solomon mutwiri mioro <solomioro@gmail.com>
2024-06-27 22:08:16 +0000

    deleted oast

--- myApp.js
+++ myApp.js
@@ -2,10 +2,6 @@ let express = require('express');
 let app = express();
 
 
-//console.log('Hello World')
-//app.get('/', function(req, res){
-//   res.send("Hello Express")
-//})
 app.get('/', (res,req){
     const absolutePath = __dirname + '/views/index.html';
     res.sendFile(absolutePath);
