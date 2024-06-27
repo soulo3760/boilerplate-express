1f0b8f8a4c93dba344de9a29ef1bb987397cf943
solomon mutwiri mioro <solomioro@gmail.com>
2024-06-27 22:13:20 +0000

    arrow function

--- myApp.js
+++ myApp.js
@@ -2,10 +2,10 @@ let express = require('express');
 let app = express();
 
 
-app.get('/', function(res,req){
-    const absolutePath = __dirname + '/views/index.html';
-    res.sendFile(absolutePath);
-})
+app.get('/', (res, req) => {
+        const absolutePath = __dirname + '/views/index.html';
+        res.sendFile(absolutePath);
+    })
 
 
 
