02ecbe4128447ccdcd140d68aa89604ec40d13fa
solomon mutwiri mioro <solomioro@gmail.com>
2024-06-27 22:25:27 +0000

    adjusted indexhtml

--- myApp.js
+++ myApp.js
@@ -1,11 +1,11 @@
 let express = require('express');
+const { sendFile } = require('express/lib/response');
 let app = express();
 
 
-app.get('/', (res, req) => {
-        const absolutePath = __dirname + '/views/index.html';
-        res.sendFile(absolutePath);
-    })
+app.get("/", (req, res) {
+sendFile(__dirname + "/views/index.html");
+    });
 
 
 
